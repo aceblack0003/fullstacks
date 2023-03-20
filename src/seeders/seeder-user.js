c247165d120da792805e52a23595383746742bf4
aceblack0003 <aceblack0003@gmail.com>
2023-03-20 23:11:03 +0700

    fullstack-bookingcare

--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = {
+
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@gmail.com',
+      password: '123456',
+      firstName: 'Hieu',
+      lastName: 'Aceblack0001',
+      address: 'DaNang',
+      gender: '1',
+      typeRole: 'Role',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
